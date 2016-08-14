f7e9ba5fbc3ebad4f8a035d3c909a7c961e3e4bc
Bill Clinton <BillClinton@users.noreply.github.com>
2016-08-13 23:18:52 -0400

    Add toolbar to editor window

--- sencha-workspace/EmergenceDbtool/app/view/Editor.js
+++ sencha-workspace/EmergenceDbtool/app/view/Editor.js
@@ -7,10 +7,36 @@ Ext.define('EmergenceDbtool.view.Editor', {
 
     layout: 'fit',
 
+    dockedItems: [{
+        xtype: 'toolbar',
+        dock: 'top',
+        items: [{
+            xtype: 'combobox',
+            queryMode: 'local',
+            displayField: 'Name',
+            store: {
+                type: 'chained',
+                source: 'Databases',
+                filters: [
+                    function(rec) {
+                        return (rec.get('mtype')==='EmergenceDbtool.model.Database');
+                    }
+                ]
+            }
+        },{
+            xtype: 'button',
+            text: 'Execute',
+            action: 'execute-sql'
+        }]
+    }],
+
     items: [{
         xtype: 'jarvus-monaco-editor',
         language: 'sql',
         source: 'dev',
+        subscribe: [
+            'onMouseDown'
+        ],
         content: [
             'SELECT ID, FirstName, Lastname',
             'FROM people',
